b8f4cd57d2e1563802b6acbdad5e7274c1f15c79
Abhishek Soni <abhisheksoni.coder@gmail.com>
2025-06-23 14:49:09 +0530

    feat: support sum of comma-separated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,8 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty?
-    return input.to_i
+    return input.to_i if input.match(/^\d+$/)
+
+    input.split(",").map(&:to_i).sum
   end
 end
