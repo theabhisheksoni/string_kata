0b074fbcc2bcbb8d0681c8e8610352b283fb28c1
Abhishek Soni <abhisheksoni.coder@gmail.com>
2025-06-23 15:20:51 +0530

    feat: raise error if input contains negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,11 +4,13 @@ class StringCalculator
     if input.start_with?("//")
       delimiter, input = input[2..].split("\n", 2)
       delimiters = [Regexp.escape(delimiter)]
-      return input.split(Regexp.union(delimiters)).map(&:to_i).sum
+      nums = input.split(Regexp.union(delimiters)).map(&:to_i)
+    else
+      string = input.gsub(/\s+/, ",")
+      nums = string.split(",").map(&:to_i)
     end
-    string = input.gsub(/\s+/, ",")
-
-    numbers = string.split(",").map(&:to_i)
-    return numbers.sum
+    negatives = nums.select { |n| n < 0 }
+    raise "Negatives not allowed: #{negatives.join(',')}" if negatives.any?
+    return nums.sum
   end
 end
