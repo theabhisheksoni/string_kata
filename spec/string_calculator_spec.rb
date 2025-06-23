ac44a0ed12c64e941f8729db60bfb20109733add
Abhishek Soni <abhisheksoni.coder@gmail.com>
2025-06-23 15:05:41 +0530

    test: add test for custom delimiter with //;\n syntax

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe StringCalculator do
     it "supports newline as delimiter" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it "supports custom single-character delimiter" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
